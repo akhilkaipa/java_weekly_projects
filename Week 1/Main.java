71e00fa7f857b6f341ae4fa11be43c4050e8312f
akhilkaipa <akhilkaipa8@gmail.com>
2025-03-07 02:24:41 +0100

    Create Main.java

---
+++ Week 1/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello everyone");
+        System.out.printf("Hello Vistula!%n");
+        System.out.println("Program executed succesfully.");
+    }
+}
